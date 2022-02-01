64030d2695a3975e163d63bc11dfbcfac3819272
RavitHun <77621123+Ravit27-N@users.noreply.github.com>
2022-02-01 23:02:45 +0700

    Web 1/2/2021

--- js/profiledisplay.js
+++ js/profiledisplay.js
@@ -1,20 +1,23 @@
 
 $(document).ready(function () { 
-// Get the modal
-var modal = document.getElementById("myModal");
+      // Get the modal
+      var modal = document.getElementById("myModal");
 
-var modalImg = document.getElementById("img01");
+      var modalImg = document.getElementById("img01");
 
-$("#myImg").click(function(){
-  modal.style.display = "block";
-  modalImg.src = "images/myprofile.jpg";
-})
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+      $("#myImg").click(function(){
+        modal.style.display = "block";
+        modalImg.src = "images/myprofile.jpg";
+      })
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() { 
+        modal.style.display = "none";
+      }
 
+      $("#submitbtn").click(function(){
+        alert('Thank you for your contact.')
+      })
 })
 
